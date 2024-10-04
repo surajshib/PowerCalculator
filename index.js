06f86fb7f0247f3aa934bcf623ffb41892953455
Suraj Singla <suraj.singla@shib.io>
2024-10-04 19:48:55 +0530

    fixes

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
-// https://powercalculator-1dgm.onrender.com/
-// http://localhost:3000/?campaignId=0x104f1f6583520c5b5d6c24bd49605f86d140940017f04fc04d2e2cf440919873&sender=0x9c86927934f9BD0184515681C44eE73e9f05c5c9
 app.get("/", async (req, res) => {
-  try{
+  try {
+    let { campaignId, sender } = req.query;
 
-    const { campaignId, sender } = req.query;
-    if (!campaignId || !sender) res.send({ data: 0 });
+    if (!sender && campaignId) {
+      let data = campaignId.split("=");
+      sender = data[1];
+      data = data[0].split("\\u0026");
+      campaignId = data[0];
+    }
+
+    if (!campaignId || !sender) res.send({ data: 0, error: "No parameters" });
     else {
       const votingPower = await calculateVotingPower(campaignId, sender);
       res.send({ data: votingPower });
     }
-  } catch(e) {
+  } catch (e) {
     console.log(`Error caught: `, e);
-    res.send({ data: 0, isError: true});
+    res.send({ data: 0, isError: true });
   }
 });
 
